b6980566a5c403036186dc279669ac7aeb23283f
adamsmigielski <adam.smigielski@gmail.com>
2015-10-07 14:42:21 +0200

    Moved platforms.cmake

--- platforms/platforms.cmake
+++
@@ -1,9 +0,0 @@
-# Select target platform
-IF(WIN32)
-    SET(PLATFORM "win_32_64")
-ENDIF(WIN32)
-    
-
-# Include target-specific definitions
-INCLUDE_DIRECTORIES(platforms/${PLATFORM})
-ADD_SUBDIRECTORY(platforms/${PLATFORM})
